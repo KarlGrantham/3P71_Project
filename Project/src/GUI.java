34d3b9529afd0519dd0a7019253d6917cb212dbe
EvanDelo <ejmdelo@gmail.com>
2019-01-05 17:36:09 -0500

    Update GUI.java

--- Project/src/GUI.java
+++ Project/src/GUI.java
@@ -1,4 +1,4 @@
-
+package project;
 
 import java.awt.Color;
 import java.awt.Dimension;
@@ -18,11 +18,13 @@ public class GUI {
     JPanel panel;
     JFrame frame;
     JButton[][] buttonList = new JButton[8][8];
+    boolean currTeam = true;
+    LinkedList<Coordinate> currentMoveSet = new LinkedList<>();
+    Coordinate lastPieceClicked;
 
     public GUI(Board b) {
-
         this.b = b;
-        initializeBoard();
+        initializeBoard(true);
         frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(new Dimension(500, 500));
@@ -32,21 +34,20 @@ public class GUI {
         frame.setVisible(true);
     }
 
-    private void initializeBoard() {
+    private void initializeBoard(boolean team) {
         panel = new JPanel(new GridLayout(0, 8));
         panel.setBackground(Color.WHITE);
         for (int y = 0; y < 8; y++) {
             for (int x = 0; x < 8; x++) {
                 JButton button = new JButton();
-                String path = "C:\\IMAGES\\";
-                if (b.getBoard()[x][y] != null && b.getBoard()[x][y].team == true) {
+                String path = "D:\\Desktop_HDD\\IMAGES\\";
+                if (b.getBoard()[x][y] != null && b.getBoard()[x][y].team == team) {
                     button.setIcon(new ImageIcon(path + b.getBoard()[x][y].imageName + ".png"));
                     button.setDisabledIcon(new ImageIcon(path + b.getBoard()[x][y].imageName + ".png"));
-
-                } else if (b.getBoard()[x][y] != null && b.getBoard()[x][y].team == false) {
+                } else if (b.getBoard()[x][y] != null && b.getBoard()[x][y].team != team) {
                     button.setIcon(new ImageIcon(path + b.getBoard()[x][y].imageName + ".png"));
                     button.setDisabledIcon(new ImageIcon(path + b.getBoard()[x][y].imageName + ".png"));
-                    //button.setEnabled(false);
+                    button.setEnabled(false);
                 } else {
                     button.setEnabled(false);
                 }
@@ -67,33 +68,78 @@ public class GUI {
     }
 
     private class ButtonClicked implements ActionListener {
-
+        
         @Override
         public void actionPerformed(ActionEvent e) {
             JButton btn = (JButton) e.getSource();
-            for (int y = 0; y < 8; y++) {
-                for (int x = 0; x < 8; x++) {
-                    if ((x % 2 != 1 && y % 2 != 1) || (x % 2 != 0 && y % 2 != 0)) {
-                        buttonList[x][y].setBackground(Color.GRAY);
-                    } else {
-                        buttonList[x][y].setBackground(Color.WHITE);
-                    }
-                }
-            }
             int x = (int) btn.getClientProperty("x");
             int y = (int) btn.getClientProperty("y");
-//            System.out.println(x + " " + y);
-            LinkedList<Coordinate> possibleMoves = b.getBoard()[x][y].moves(x, y, b.getBoard());
-            btn.setBackground(Color.RED);
-//            System.out.println(b.getBoard()[x][y].getClass().getName());
-            while (!possibleMoves.isEmpty()) {
-//                System.out.println(possibleMoves.get(0).x + " " + possibleMoves.get(0).y);
-
-                buttonList[possibleMoves.get(0).x][possibleMoves.get(0).y].setBackground(Color.BLUE);
-                buttonList[possibleMoves.get(0).x][possibleMoves.get(0).y].setEnabled(true);
-                possibleMoves.remove(0);
+            boolean moveSelected = false;
+            for (int i = 0; i < currentMoveSet.size(); i++) {
+                if (currentMoveSet.get(i).x == x && currentMoveSet.get(i).y == y) {
+                    moveSelected = true;
+                    break;
+                } else {
+                    moveSelected = false;
+                }
             }
-            
+            if (moveSelected) {// if the button clicked was a possible move
+                currTeam = !currTeam;
+                b = b.move(lastPieceClicked, new Coordinate(x,y));
+                String path = "D:\\Desktop_HDD\\IMAGES\\";
+                for (int yCoor = 0; yCoor < 8; yCoor++) {
+                    for (int xCoor = 0; xCoor < 8; xCoor++) {
+                        if (b.getBoard()[xCoor][yCoor] != null && b.getBoard()[xCoor][yCoor].team == currTeam) {
+                            buttonList[xCoor][yCoor].setIcon(new ImageIcon(path + b.getBoard()[xCoor][yCoor].imageName + ".png"));
+                            buttonList[xCoor][yCoor].setDisabledIcon(new ImageIcon(path + b.getBoard()[xCoor][yCoor].imageName + ".png"));
+                            buttonList[xCoor][yCoor].setEnabled(true);
+                        } else if (b.getBoard()[xCoor][yCoor] != null && b.getBoard()[xCoor][yCoor].team != currTeam) {
+                            buttonList[xCoor][yCoor].setIcon(new ImageIcon(path + b.getBoard()[xCoor][yCoor].imageName + ".png"));
+                            buttonList[xCoor][yCoor].setDisabledIcon(new ImageIcon(path + b.getBoard()[xCoor][yCoor].imageName + ".png"));
+                            buttonList[xCoor][yCoor].setEnabled(false);
+                        } else {
+                            buttonList[xCoor][yCoor].setIcon(null);
+                            buttonList[xCoor][yCoor].setEnabled(false);
+                        }
+                        if ((xCoor % 2 != 1 && yCoor % 2 != 1) || (xCoor % 2 != 0 && yCoor % 2 != 0)) {
+                            buttonList[xCoor][yCoor].setBackground(Color.GRAY);
+                        } else {
+                            buttonList[xCoor][yCoor].setBackground(Color.WHITE);
+                        }
+                    }
+                }
+            currentMoveSet.clear();
+            } else { // button clicked was a piece
+                lastPieceClicked = new Coordinate(x, y);
+                for (int yCoor = 0; yCoor < 8; yCoor++) {
+                    for (int xCoor = 0; xCoor < 8; xCoor++) {
+                        if (b.getBoard()[xCoor][yCoor] != null) {
+                            if (b.getBoard()[xCoor][yCoor].team == currTeam) {
+                                buttonList[xCoor][yCoor].setEnabled(true);
+                            } else if (b.getBoard()[xCoor][yCoor].team != currTeam) {
+                                buttonList[xCoor][yCoor].setEnabled(false);
+                            }
+                        } else {
+                            buttonList[xCoor][yCoor].setEnabled(false);
+                        }
+                        if ((xCoor % 2 != 1 && yCoor % 2 != 1) || (xCoor % 2 != 0 && yCoor % 2 != 0)) {
+                            buttonList[xCoor][yCoor].setBackground(Color.GRAY);
+                        } else {
+                            buttonList[xCoor][yCoor].setBackground(Color.WHITE);
+                        }
+                    }
+                }
+                LinkedList<Coordinate> possibleMoves = b.getBoard()[x][y].moves(x, y, b.getBoard());
+                btn.setBackground(Color.RED);
+                while (!possibleMoves.isEmpty()) {
+                    currentMoveSet.add(possibleMoves.get(0));
+                    //System.out.println(possibleMoves.get(0).x + " " + possibleMoves.get(0).y);
+                    buttonList[possibleMoves.get(0).x][possibleMoves.get(0).y].setBackground(Color.BLUE);
+                    buttonList[possibleMoves.get(0).x][possibleMoves.get(0).y].setEnabled(true);
+                    possibleMoves.remove(0);
+                }
+            }
+
         }
     }
 
